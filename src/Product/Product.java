84a1ad3da565a806a32d8621a70859d485a96021
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-03-24 23:05:09 +0600

    modify product and specification class

--- src/Product/Product.java
+++ src/Product/Product.java
@@ -1,12 +1,98 @@
 package Product;
 
+import Product.enums.Status;
+
 public class Product {
-//	private final String productName;
-//	private final String productType;
+	private String name;
+	private String title;
+	private String type;
 	private String brand;
-	private String productStatus;
+	private String summary;
+	private String description;
+	private Status status;
 
-//	private String features = new String();
-//	private String[] FAQ = new String[5];
+	private final String[] features = new String[20];
+	private final String[] FAQ = new String[5];
+	private final Status[] statuses = {Status.UPCOMING, Status.IN_STOCK, Status.OUT_OF_STOCK};
 
+	public Product() {
+		this.name = "null";
+		this.title = "null";
+		this.type = "null";
+		this.brand = "null";
+		this.summary = "null";
+		this.description = "null";
+		this.status = Status.UPCOMING;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Status getCurrentStatus() {
+		return status;
+	}
+
+	public Status getStatus(int index) {
+		return statuses[index - 1];
+	}
+
+	public void setStatus(Status status) {
+			this.status = status;
+	}
+
+	public String viewStatuses() {
+		StringBuilder statusesString = new StringBuilder();
+		int i = 1;
+
+		for (Status status: this.statuses) {
+			statusesString.append(i++).append(".").append(status).append('\n');
+		}
+
+		return statusesString.toString();
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- src/Product/Specification.java
+++ src/Product/Specification.java
@@ -1,6 +1,38 @@
 package Product;
 
+import Product.enums.Warranty;
+
 public class Specification {
 	private final KeyFeatures keyFeatures = new KeyFeatures();
 	private final PhysicalAttribute physicalAttribute = new PhysicalAttribute();
+	private Warranty warranty;
+
+	private final Warranty[] warranties = {Warranty.NO_WARRANTY, Warranty.YEAR_WARRANTY, Warranty.YEARS_WARRANTY, Warranty.LIFETIME_WARRANTY};
+
+	public Specification() {
+		this.warranty = Warranty.NO_WARRANTY;
+	}
+
+	public Warranty getWarranty() {
+		return this.warranty;
+	}
+
+	public Warranty getWarranty(int index) {
+		return this.warranties[index - 1];
+	}
+
+	public void setWarranty(Warranty warranty) {
+		this.warranty = warranty;
+	}
+
+	public String viewWarranties() {
+		StringBuilder warrantyString = new StringBuilder();
+		int i = 1;
+
+		for (Warranty warranty: this.warranties) {
+			warrantyString.append(i++).append(".").append(warranty).append('\n');
+		}
+
+		return warrantyString.toString();
+	}
 }
